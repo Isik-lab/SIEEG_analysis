ab47d570c5b458548a356c6af9fbeb53a5c21fa2
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-10-20 12:27:13 -0400

    parallel

--- scripts/pairwise_decoding.py
+++ scripts/pairwise_decoding.py
@@ -8,6 +8,19 @@ from pathlib import Path
 import argparse
 import pandas as pd
 import scipy
+from concurrent.futures import ProcessPoolExecutor
+
+
+def worker(t, c1, c2, v1, v2, X_train, X_test, y_train, y_test):
+    model = LinearSVC()
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    accuracy = np.mean(y_test == predictions)
+    return {'c1': c1, 'c2': c2, 'time': t, 'accuracy': accuracy, 'v1': v1, 'v2': v2}
+
+
+def unpack_and_call_worker(args):
+    return worker(*args)
 
 
 class PairwiseDecoding:
@@ -45,40 +58,53 @@ class PairwiseDecoding:
         end = time.time()
         print(f'computing pseudo-trials took {end-start:0f} s.')
 
-        # 2. Whitening using the Epoch method
-        print('beginning whitening...')
-        start = time.time()
-        sigma_conditions = data['labels_pseudo_train'][0, :, data['n_pseudo']-1:].flatten()
-        sigma_ = np.empty((data['n_conditions'], data['n_sensors'], data['n_sensors']))
-        for c in data['conditions']:
-            # compute sigma for each time point, then average across time
-            sigma_[c] = np.mean([_cov(Xpseudo_train[sigma_conditions==c, :, t], shrinkage='auto')
-                                    for t in range(data['n_time'])], axis=0)
-        sigma = sigma_.mean(axis=0)  # average across conditions
-        sigma_inv = scipy.linalg.fractional_matrix_power(sigma, -0.5)
-        Xpseudo_train = (Xpseudo_train.swapaxes(1, 2) @ sigma_inv).swapaxes(1, 2)
-        Xpseudo_test = (Xpseudo_test.swapaxes(1, 2) @ sigma_inv).swapaxes(1, 2)
-        end = time.time()
-        print(f'whitening took {end-start:0f} s.')
+        ## 2. Whitening using the Epoch method
+        # print('beginning whitening...')
+        # start = time.time()
+        # sigma_conditions = data['labels_pseudo_train'][0, :, data['n_pseudo']-1:].flatten()
+        # sigma_ = np.empty((data['n_conditions'], data['n_sensors'], data['n_sensors']))
+        # for c in data['conditions']:
+        #     # compute sigma for each time point, then average across time
+        #     sigma_[c] = np.mean([_cov(Xpseudo_train[sigma_conditions==c, :, t], shrinkage='auto')
+        #                             for t in range(data['n_time'])], axis=0)
+        # sigma = sigma_.mean(axis=0)  # average across conditions
+        # sigma_inv = scipy.linalg.fractional_matrix_power(sigma, -0.5)
+        # Xpseudo_train = (Xpseudo_train.swapaxes(1, 2) @ sigma_inv).swapaxes(1, 2)
+        # Xpseudo_test = (Xpseudo_test.swapaxes(1, 2) @ sigma_inv).swapaxes(1, 2)
+        # end = time.time()
+        # print(f'whitening took {end-start:0f} s.')
        
         # 3. Decoding and prediction
-        model = LinearSVC()
-        print('computing pairwise distances...')
-        result = []
+        # Prepare tasks
+        print('preparing tasks for parallel processing...')
+        start = time.time()
+        tasks = []
         for t_i, t in tqdm(enumerate(data['time']), total=data['n_time']):
             for (c1, c2), (v1, v2) in zip(conditions_nCk, videos_nCk):
-                train_idx = ind_pseudo_train[c1, c2]
-                test_idx = ind_pseudo_test[c1, c2]
-                
-                model.fit(Xpseudo_train[train_idx, :, t_i], labels_pseudo_train[c1, c2])
-                predictions = model.predict(Xpseudo_test[test_idx, :, t_i])
-                accuracy = np.mean(labels_pseudo_test[c1, c2] == predictions)
-                result.append({'c1': c1, 'c2': c2, 'time': t, 'accuracy': accuracy,
-                               'v1': v1, 'v2': v2, 'perm': self.perm})
-                
+                y_train = labels_pseudo_train[c1, c2]
+                y_test = labels_pseudo_test[c1, c2]
+                X_train = Xpseudo_train[ind_pseudo_train[c1, c2], :, t_i]
+                X_test = Xpseudo_test[ind_pseudo_test[c1, c2], :, t_i]
+                tasks.append((t, c1, c2, v1, v2, X_train, X_test, y_train, y_test))
+        end = time.time()
+        print(f'preparing tasks took {end-start:0f} s.')
+
+        # Execute tasks in parallel
+        print('now beginning the tasks...')
+        start = time.time()
+        results = []
+        with ProcessPoolExecutor(max_workers=4) as executor:
+            for result in tqdm(executor.map(unpack_and_call_worker, tasks), total=len(tasks)):
+                results.append(result)
+        end = time.time()
+        print(f'completing the tasks took {end-start:0f} s.')
+        
+        # 4. Saving
         print('saving...')
-        df = pd.DataFrame(result)
-        df.to_csv(f'{self.data_dir}/{self.process}/{self.sid}/rdm_perm-{self.permutation_number}.csv', index=False)
+        df = pd.DataFrame(results)
+        df['perm'] = self.perm
+        df.to_csv(f'{self.data_dir}/{self.process}/{self.sid}/rdm_perm-{self.permutation_number}.csv.gz',
+                   index=False, compression='gzip')
         print('Finished!')
 
 def main():
