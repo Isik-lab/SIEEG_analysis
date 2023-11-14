8e9f72e87937bd91d07429de26347c1fc50f3a56
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2023-11-14 16:10:23 -0500

    sbatch

--- scripts/batch_fmri_decoding.sh
+++ scripts/batch_fmri_decoding.sh
@@ -3,10 +3,9 @@
 #SBATCH
 #SBATCH --job-name=glmsingle
 #SBATCH --time=02:00:00
-#SBATCH --partition=a100
-#SBATCH -A lisik3_gpu
+#SBATCH --partition=defq
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=12
 #SBATCH --mail-type=end
 #SBATCH --mail-user=emcmaho7@jh.edu
 
