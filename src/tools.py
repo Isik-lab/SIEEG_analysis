906b40dad1ba1088e0309fbe0b22bb4503b80232
emaliemcmahon@gmail.com <emaliemcmahon@gmail.com>
2024-07-30 11:39:36 -0400

    whole brain plotting

--- src/tools.py
+++ src/tools.py
@@ -14,3 +14,48 @@ def to_numpy(tensors):
         return [to_numpy(tensor) for tensor in tensors]
     else:
         return tensors.cpu().detach().numpy()
+    
+
+def camera_switcher(hemi, view):
+    if view == 'lateral':
+        if hemi == 'lh':
+            camera = dict(
+                up=dict(x=0, y=0, z=1),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=-1.5, y=0, z=0)
+            )
+        else:
+            camera = dict(
+                up=dict(x=0, y=0, z=1),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=1.5, y=0, z=0)
+            )
+    elif view == 'medial':
+        if hemi == 'lh':
+            camera = dict(
+                up=dict(x=0, y=0, z=1),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=1.75, y=0, z=0)
+            )
+        else:
+            camera = dict(
+                up=dict(x=0, y=0, z=1),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=-1.75, y=0, z=0)
+            )
+    elif view == 'ventral':
+        if hemi == 'lh':
+            camera = dict(
+                up=dict(x=0, y=1, z=0),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=0, y=0, z=-2.5)
+            )
+        else:
+            camera = dict(
+                up=dict(x=0, y=1, z=0),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=0, y=0, z=-2.5)
+            )
+    else:
+        raise 'invalid view'
+    return camera
